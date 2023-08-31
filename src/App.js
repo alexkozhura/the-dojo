6b4e5a93babc364ba5d585d5d3cb7d99a07ec913
alexkozhura <alexkozhura@gmail.com>
2023-08-31 18:49:58 +0200

    Added Firebase storage

--- src/App.js
+++ src/App.js
@@ -9,21 +9,23 @@ import Project from './pages/project/Project'
 import Signup from './pages/signup/Signup'
 import Login from './pages/login/Login'
 import Navbar from './components/Navbar'
+import Sidebar from './components/Sidebar'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <div className="container">
-          <Navbar />
-            <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/create" element={<Create />} />
-              <Route path="/project/:id" element={<Project />} />
-              <Route path="/signup" element={<Signup />} />
-              <Route path="/login" element={<Login />} />
-            </Routes>
-        </div>
+        <Sidebar />
+          <div className="container">
+            <Navbar />
+              <Routes>
+                <Route path="/" element={<Dashboard />} />
+                <Route path="/create" element={<Create />} />
+                <Route path="/project/:id" element={<Project />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+          </div>
       </BrowserRouter>
     </div>
   );
