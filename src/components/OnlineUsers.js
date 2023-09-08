363706d73aa3540d5b83caa4eb587911e96ce152
alexkozhura <alexkozhura@gmail.com>
2023-09-08 19:00:46 +0200

    Added OnlineUsers.css

---
+++ src/components/OnlineUsers.js
@@ -0,0 +1,20 @@
+import { useCollection } from '../hooks/useCollection'
+import Avatar from '../components/Avatar'
+
+import './OnlineUsers.css'
+
+export default function OnlineUsers() {
+  const { documents, error } = useCollection('users')
+  return (
+    <div className="user-list">
+        <h2>All users</h2>
+        {error && <div className={error}>{error}</div>}
+        {documents && documents.map(user => (
+          <div key={user.id} className='user-list-item'>
+            <span>{user.displayName}</span>
+            <Avatar src={user.photoURL} />
+          </div>
+        ))}
+    </div>
+  )
+}
