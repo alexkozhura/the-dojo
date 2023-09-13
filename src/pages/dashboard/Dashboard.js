4a36d57e8bf08db4b28b2847e7c8c4d524ab1c32
alexkozhura <alexkozhura@gmail.com>
2023-09-13 19:30:08 +0200

    Added ProjectList component

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -1,9 +1,17 @@
+import { useCollection } from '../../hooks/useCollection'
+
 import './Dashboard.css'
 
 export default function Dashboard() {
+  const { documents, error } = useCollection('projects')
   return (
     <div>
-        Dashboard
+        <h2 className="page-title">Dashboard</h2>
+        {error && <div className="error">{error}</div>}
+        {documents && documents.map(project => (
+          <p key={project.id}>{project.name}</p>
+        ))}
+
     </div>
   )
 }
